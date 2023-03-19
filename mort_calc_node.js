08eb74b896f158bc908b28c64c2b4117f1d14ee2
LeSean Bruneau <eaganshark2009@gmail.com>
2023-03-19 10:24:38 -0500

    dynamic input parameters

--- mort_calc_node.js
+++ mort_calc_node.js
@@ -1,60 +1,110 @@
+'use strict'
 
 // https://www.mtgprofessor.com/formulas.htm
 
+// node:util -> library used for text formatting
 const util = require('node:util');
 
-console.log('Testing JavaScript Program');
-
-const loanAmt = 150000
-const nbrMonths = 180
-const intRate = 0.05
+console.log(`\nMortgage interest calculator - just principle and interest monthly payments `);
+console.log(`---------------------------------------------------------------------------\n`);
 
 let monthPay = 0
 let monthIntRate = 0
 let totalIntPaid = 0
 
-monthIntRate = intRate  / 12
+let loanAmt = 1
+let nbrMonths = 1
+let intRate = 0.01
 
-let prevMonthBal = 0
-let remainBal = 0
-let monthCounter = 0
-let monthPrinPaid = 0
-let monthIntPaid = 0
+const r1 = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-monthPay = Math.round(loanAmt * (monthIntRate * Math.pow((1 + monthIntRate), nbrMonths)) / (Math.pow((1 + monthIntRate),nbrMonths) - 1 ) ) 
-console.log('Testing monthPay: ' + monthPay + '\n\n')
+const question1 = () => {
+  return new Promise((resolve, reject) => {
+    r1.question(`Enter loan amount: `, (answer) => {
+      //console.log(`Loan amount is...  ${answer}`);
+      loanAmt = answer
+      resolve()
+    })
+  })
+}
 
-console.log(' Mo  MoPay  MoPrinPd  MoIntPd  TotalIntPd  RemainBal \n')
+const question2 = () => {
+  return new Promise((resolve, reject) => {
+    r1.question(`Enter Number of Months: `, (answer) => {
+      //console.log(`Number of Months for loan is...  ${nbrMonths}`)
+      nbrMonths = answer
+      resolve()
+    })
+  })
+}
 
-for (monthCounter = 1; monthCounter <= nbrMonths; monthCounter++) {
-  prevMonthBal = 
-    Math.round(
-      loanAmt
+const question3 = () => {
+  return new Promise((resolve, reject) => {
+    r1.question(`Enter Interest Rate: `, (answer) => {
+      //console.log(`Interest rate for loan is...  ${intRate}`)
+      intRate = answer
+      resolve()
+    })
+  })
+}
+
+const calcMortPayments = () => {
+  monthIntRate = intRate  / 12
+
+  let prevMonthBal = 0
+  let remainBal = 0
+  let monthCounter = 0
+  let monthPrinPaid = 0
+  let monthIntPaid = 0
+
+  console.log(`\n--------------------------\n`);
+  console.log(`Loan amount is...  ${loanAmt}`);
+  console.log(`Number of Months for loan is...  ${nbrMonths}`);
+  console.log(`Interest rate for loan is...  ${intRate}`);
+  console.log(`\n--------------------------\n`);
+
+  monthPay = Math.round(loanAmt * (monthIntRate * Math.pow((1 + monthIntRate), nbrMonths)) / (Math.pow((1 + monthIntRate),nbrMonths) - 1 ) ) 
+  console.log('Testing monthPay: ' + monthPay + '\n\n')
+  
+  console.log(' Mo  MoPay  MoPrinPd  MoIntPd  TotalIntPd  RemainBal \n')
+
+  for (monthCounter = 1; monthCounter <= nbrMonths; monthCounter++) {
+    prevMonthBal = Math.round(loanAmt
       * ( Math.pow((1 + monthIntRate),nbrMonths) - Math.pow((1 + monthIntRate), (monthCounter - 1)) ) 
-      / ( Math.pow((1 + monthIntRate),nbrMonths) - 1 ) 
+      / ( Math.pow((1 + monthIntRate),nbrMonths) - 1 )
     )
-  // console.log('\ncounter: ' + monthCounter + '\tprevMonthBal: ' + prevMonthBal)
-  remainBal = 
-    Math.round(
-      loanAmt 
+
+    remainBal = Math.round(loanAmt
       * ( Math.pow((1 + monthIntRate),nbrMonths) - Math.pow((1 + monthIntRate), monthCounter)) 
-      / ( Math.pow((1 + monthIntRate),nbrMonths) - 1 )  
+      / ( Math.pow((1 + monthIntRate),nbrMonths) - 1 )
     )
-  // console.log('couner: ' + monthCounter + '\tremainBal: ' + remainBal)
-  monthPrinPaid = prevMonthBal - remainBal
-  monthIntPaid = monthPay - monthPrinPaid
-  totalIntPaid = totalIntPaid + monthIntPaid
-  //console.log(monthCounter + '\t' + monthPay + '\t' + monthPrinPaid + '\t' + monthIntPaid + '\t' + totalIntPaid + '\t' + remainBal)
-  console.log(util.format("%s%s%s%s%s%s",
+
+    monthPrinPaid = prevMonthBal - remainBal
+    monthIntPaid = monthPay - monthPrinPaid
+    totalIntPaid = totalIntPaid + monthIntPaid
+
+    console.log(util.format("%s%s%s%s%s%s",
                           monthCounter.toString().padStart(3),
                           monthPay.toString().padStart(7),
                           monthPrinPaid.toString().padStart(7),
                           monthIntPaid.toString().padStart(9),
                           totalIntPaid.toString().padStart(12),
                           remainBal.toString().padStart(12) ) 
-  )
+                )
 
+  }
+  
 }
 
+const main = async () => {
+  await question1()
+  await question2()
+  await question3()
+  await calcMortPayments()
+  r1.close()
+}
 
-
+main()
--- mort_calc_python.py
+++ mort_calc_python.py
@@ -1,14 +1,15 @@
 import sys
 
 monthPay = 0
-loanAmt = 200000
-nbrMonths = 180
-intRate = 0.06
-#loanAmt = 300000
-#nbrMonths = 360
-#intRate = 0.03
 monthIntRate = 0
 totalIntPaid = 0
+
+loanAmt = int(input("Enter loan amount: "))
+nbrMonths = int(input("Enter loan term (months): "))
+intRate = float(input("Enter interest rate: "))
+
+if intRate > 1:
+    intRate = intRate / 100
 
 monthIntRate = intRate / 12
 
@@ -28,7 +29,6 @@ while (monthCounter <= nbrMonths):
     totalIntPaid += monthIntPaid
     #print ('{}  {} \t {} \t {} \t {} \t {} '.format(monthCounter,monthPay,monthPrinPaid,monthIntPaid,totalIntPaid,remainBal))
     print (str(monthCounter).center(3,' ') + '  ' + str(monthPay).center(5,' ') + '  ' +
-            #str(monthPrinPaid).rjust(4,' ').ljust(8,' ') +  
             str(monthPrinPaid).center(8,' ') +  '  ' + str(monthIntPaid).center(7, ' ') + '  ' +
             str(totalIntPaid).center(10,' ') + '  ' + str(remainBal).center(9,' ') )
     monthCounter += 1
